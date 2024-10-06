ffba4978f9c615f93313f1764d85cf613506c7ba
MarianaCA11 <marian3439.lds@gmail.com>
2024-10-05 20:09:18 -0500

    cambio minimo

---
+++ Estudiante_Ingenieria.java
@@ -0,0 +1,80 @@
+public class Estudiante_Ingenieria {
+    String cedula;
+    String nombre;
+    String apellido;
+    String telefono;
+    int semestre;
+    float promedio;
+    String serial;
+    
+    public Estudiante_Ingenieria(String cedula, String nombre, String apellido, String telefono, int semestre,
+            float promedio, String serial) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.semestre = semestre;
+        this.promedio = promedio;
+        this.serial = serial;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    
+    
+
+}
